9adbfecdcbc065b91281b6724cb131de5179b0c8
7sni07 <7sni07@users.noreply.github.com>
2025-11-17 20:54:09 +0000

    deploy: 2c2db34d043d4e4ae665ff3671219f173d5a184f

---
+++ html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['getinstance_0',['getInstance',['../classDatabase.html#a8399325825e893103c463e644f817f86',1,'Database']]],
+  ['getpdo_1',['getPdo',['../classDatabase.html#a764343ea3b46daad2deec6f8e0a39566',1,'Database']]]
+];
