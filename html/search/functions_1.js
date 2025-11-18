86bb44a55789441746280be467b89d2ce6fce7ff
7sni07 <7sni07@users.noreply.github.com>
2025-11-18 18:29:04 +0000

    deploy: cc1ab78985aa1fa62fe2b779ebd286a34ff14431

--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['creategame_0',['createGame',['../classGame.html#aac39ebf78457b6b2c1c8bd06582a94ab',1,'Game']]],
+  ['creategame_0',['createGame',['../classGame.html#aa15f8414a3840928edb20b58f7a4a962',1,'Game']]],
   ['createplayer_1',['createPlayer',['../classGame.html#a7a930ce861ff251a89c85543719f6a2e',1,'Game']]]
 ];
