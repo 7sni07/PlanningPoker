4acfcaf3450167caddf7990b272d084e834007ce
7sni07 <7sni07@users.noreply.github.com>
2025-12-17 15:47:58 +0000

    deploy: ab4114ed74225f146e649e374b55c262a711a542

--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['getbacklogitems_0',['getBacklogItems',['../classGame.html#a7e593761c88de15ccda73d260080254b',1,'Game']]],
   ['getgameinfo_1',['getGameInfo',['../classGame.html#a6e40688e8728d29da2364fadba9602d0',1,'Game']]],
-  ['getinstance_2',['getInstance',['../classDatabase.html#a8399325825e893103c463e644f817f86',1,'Database']]],
-  ['getnextpendingtask_3',['getNextPendingTask',['../classGame.html#a5920f653d7d1e685533edc83c3a73e4a',1,'Game']]],
-  ['getpdo_4',['getPdo',['../classDatabase.html#a764343ea3b46daad2deec6f8e0a39566',1,'Database']]],
-  ['getvotesforround_5',['getVotesForRound',['../classGame.html#a4867078cb73bb1d3336a74e2d0b2bc73',1,'Game']]]
+  ['gethostplayer_2',['getHostPlayer',['../classGame.html#a681ae365e444002b56ccc39ef499ee50',1,'Game']]],
+  ['getinstance_3',['getInstance',['../classDatabase.html#a8399325825e893103c463e644f817f86',1,'Database']]],
+  ['getnextpendingtask_4',['getNextPendingTask',['../classGame.html#a5920f653d7d1e685533edc83c3a73e4a',1,'Game']]],
+  ['getpdo_5',['getPdo',['../classDatabase.html#a764343ea3b46daad2deec6f8e0a39566',1,'Database']]],
+  ['getvotesforround_6',['getVotesForRound',['../classGame.html#a4867078cb73bb1d3336a74e2d0b2bc73',1,'Game']]]
 ];
